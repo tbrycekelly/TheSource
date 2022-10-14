8ebbd89fb199b21d36d13c2389e958ba77322d20
Thomas Bryce Kelly <tbrycekelly@gmail.com>
2022-10-14 09:57:21 -0800

    Update map.plot.R

--- R/map.plot.R
+++ R/map.plot.R
@@ -337,8 +337,8 @@ get.map.line = function(map,
 
   if (length(lon) != length(lat)) {stop('get.map.line: length of lon/lat are not the same.')}
   if (greatCircle) {
-    lons = approx(1:length(lon), lon, xout = seq(1, length(lon), length.out = max(length(lon), 5e4)))$y
-    lats = approx(1:length(lat), lat, xout = seq(1, length(lat), length.out = max(length(lon), 5e4)))$y
+    lons = approx(1:length(lon), lon, xout = seq(1, length(lon), length.out = max(length(lon), 1e4)))$y
+    lats = approx(1:length(lat), lat, xout = seq(1, length(lat), length.out = max(length(lon), 1e4)))$y
   } else {
     lons = lon
     lats = lat
@@ -469,7 +469,7 @@ add.map.axis.label = function(map, temp, label, sides) {
     if (min(abs(temp[,2] - usr[3])) < dx) {
 
       ## Check angle
-      k = which.min((temp[-nrow(temp),2] - usr[3])^2)
+      k = which.min(abs(temp[-nrow(temp),2] - usr[3]))
       dd = abs(diff(temp)[k,])
       if (dd[1]/dd[2] < 2 * dy / dx) {
         axis(1, at = temp[which.min((temp[,2] - usr[3])^2),1], labels = label)
@@ -482,7 +482,7 @@ add.map.axis.label = function(map, temp, label, sides) {
     if (min(abs(temp[,2] - usr[4])) < dx) {
 
       ## Check angle
-      k = which.min((temp[-nrow(temp),2] - usr[4])^2)
+      k = which.min(abs(temp[-nrow(temp),2] - usr[4]))
       dd = abs(diff(temp)[k,])
       if (dd[1]/dd[2] < 2 * dy / dx) {
         axis(3, at = temp[which.min((temp[,2] - usr[4])^2),1], labels = label)
@@ -495,7 +495,7 @@ add.map.axis.label = function(map, temp, label, sides) {
     if (min(abs(temp[,1] - usr[1])) < dy) {
 
       ## Check angle
-      k = which.min((temp[-nrow(temp),1] - usr[1])^2)
+      k = which.min(abs(temp[-nrow(temp),1] - usr[1]))
       dd = abs(diff(temp)[k,])
       if (dd[2]/dd[1] < 2 * dx / dy) {
         axis(2, at = temp[which.min((temp[,1] - usr[1])^2),2], labels = label, las = 2)
@@ -553,7 +553,7 @@ add.map.axis = function(map, lons = NULL, lats = NULL, sides = c(1:4)) {
 #' @title Add Grid to Map
 #' @author Thomas Bryce Kelly
 #' @export
-add.map.grid = function(map, lons = NULL, lats = NULL, col = 'darkgrey', lty = 2, lwd = 1) {
+add.map.grid = function(map, lons = NULL, lats = NULL, col = 'darkgrey', lty = 1, lwd = 1) {
   
   if (is.null(lons)) { lons = seq(-180, 180, by = map$grid$dlon) }
   if (is.null(lats)) { lats = seq(-90, 90, by = map$grid$dlat) }
